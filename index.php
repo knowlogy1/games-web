852dcaa6ecf7d6e2027c1ec0d22cb17563e50d74
knowlogy1 <84484434+knowlogy1@users.noreply.github.com>
2025-07-30 17:02:50 -1200

    games web
    game web pages you can used the miniweb to open it offline.i used the old ps3 method

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<link href="jQueryAssets/jquery.ui.core.min.css" rel="stylesheet" type="text/css">
+<link href="jQueryAssets/jquery.ui.theme.min.css" rel="stylesheet" type="text/css">
+<link href="jQueryAssets/jquery.ui.button.min.css" rel="stylesheet" type="text/css">
+<script src="jQueryAssets/jquery-1.11.1.min.js"></script>
+<script src="jQueryAssets/jquery.ui-1.10.4.button.min.js"></script>
+</head><table width="1078" border="1">
+  <tbody>
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+  </tbody>
+</table><<?php $_GET[www.knowlogygame.com]; ?>
+
+
+<body>
+<img src="apn network.gif" width="1080" height="200" alt<script type="text/javascript">
+$(function() {
+	$( yuu).button(); 
+});
+</script>
+</body>
+</html>
